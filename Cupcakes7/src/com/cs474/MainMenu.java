cc5c34c3a52c6b25e5396798ebfd6eb62f942b70
roberteblackwood@gmail.com <roberteblackwood@gmail.com@4e9b3a32-12ef-d037-27d2-596e81d57c36>
2011-04-20 21:14:31 +0000

    Added recipe activity and data passing through sharedpreference object

--- Cupcakes7/src/com/cs474/MainMenu.java
+++ Cupcakes7/src/com/cs474/MainMenu.java
@@ -4,13 +4,18 @@ import com.cs474.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainMenu extends Activity {
     /** Called when the activity is first created. */
+    public static final String PREFS_NAME = "MyPrefsFile";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +23,33 @@ public class MainMenu extends Activity {
         
         final Button logButton = (Button) findViewById(R.id.logButton);
         final Button ingredientButton = (Button) findViewById(R.id.ingredientButton);
+        final Button recipeButton = (Button) findViewById(R.id.recipeButton);
+        final Button menuButton = (Button) findViewById(R.id.menuButton);
+        final SharedPreferences appData = getSharedPreferences(PREFS_NAME, 0);
+        
         ingredientButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Intent i = new Intent(MainMenu.this, IngredientsActivity.class);
+	        	i.putExtra("MY_CALLER", "MainMenu");
 		        startActivity(i);
 			}	    	
 	    });
+        
+        recipeButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(MainMenu.this, RecipesActivity.class);
+	        	i.putExtra("MY_CALLER", "MainMenu");
+		        startActivity(i);
+			}	    	
+	    });
+        
+        menuButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				//String recipeIngr = appData.getString("SEL_INGREDIENT", "");
+				//((TextView) findViewById(R.id.mainHeader)).setText(recipeIngr);
+			}	    	
+	    });
+        
         logButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 helloLog("This is MainMenu speaking.");
