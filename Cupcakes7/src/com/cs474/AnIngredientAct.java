c2d3f76c20b6d53fd053479d162993d740c11db2
roberteblackwood <roberteblackwood@gmail.com>
2011-04-20 09:53:46 +0000

    currently getting the UI flow of the app into place so that we can start writing the underlying data structures and logic.

--- Cupcakes7/src/com/cs474/AnIngredientAct.java
+++ Cupcakes7/src/com/cs474/AnIngredientAct.java
@@ -1,19 +1,29 @@
 package com.cs474;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AnIngredientAct extends Activity {
-
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    String anIngredient = getIntent().getStringExtra("AN_INGREDIENT");
 	    setContentView(R.layout.an_ingredient);
+		final Button deleteButton = (Button) findViewById(R.id.deleteButton);
 		((TextView) findViewById(R.id.ingredientName)).setText(anIngredient);
-
+		deleteButton.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
+		deleteButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}	    	
+	    });
 	    
 	}
 
